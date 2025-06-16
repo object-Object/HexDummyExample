3a3f0a45e6cb4a5133c803782765be9770ab2891
object-Object <object@objectobject.ca>
2025-06-16 11:56:04 -0400

    Add update script

---
+++ update.py
@@ -0,0 +1,13 @@
+# this file is not part of the template
+# it's just a script to update this repo when the template is updated
+
+import subprocess
+
+if __name__ == "__main__":
+    for command in [
+        ["uvx", "copier", "update", "--vcs-ref", "v2", "--skip-answered"],
+        ["git", "add", "."],
+        ["git", "commit", "-m", "Update template"],
+        ["git", "push"],
+    ]:
+        subprocess.run(command, shell=True, check=True)
